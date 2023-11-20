ddd3e16d110b5e8732dd2e3cce5913fd73a070d4
Sabahattin Mert Daloglu <smd89@cam.ac.uk>
2023-11-20 22:15:32 +0000

    Implemented a few suggestions from linter such as replacing '!= None' to 'is not None'

--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -1,55 +1,61 @@
 """
     This code solves a sudoku puzzle using backtracking algorithm.
-    
+
     Explain how it works step by step
-    
-    Backtracking algorithm is a brute force method which tries all possible values for the empty boxed until it finds the correct one.
-    The algorithm reverts back to the previous configuration of the sudoku table as soon as a contradiction is found with the current configuration.
-    
-    
-    
-    
+
+    Backtracking algorithm is a brute force method which tries
+    all possible values for the empty boxed until it finds the correct one.
+
+    The algorithm reverts back to the previous configuration of the sudoku
+    table as soon as a contradiction is found with the current configuration.
+
+
+
+
     Written by Sabahattin Mert Daloglu: smd89@cam.ac.uk
 """
 
 
-#Load Modules:
-import numpy as np
+# Load Modules:
 import sys
 from Sudoku import backtracking as bt
 from Sudoku import board as bd
 
-#bt.check_validity
 
-#bt.find_empty
-
-#bd.array_to_board
-
-
-
-#Extracting the input and the output file from the command line
+# Extracting the input and the output file from the command line
 input_file = sys.argv[1]
-f = open(input_file, "r")
-data = f.read()   #Read the text file into a string
 
 
-#output_file = sys.argv[2]
-#f.close()
+# Trapping files that can not be opened
+
+try:
+    f = open(input_file, "r")
+except IOError:
+    print("File could not be opened")
+else:
+    print("Input file opened successfully")
+
+    # Read the text file into a string
+    data = f.read()
 
 
-#Reading the input txt file and converting it to a 9x9 numpy array
+# output_file = sys.argv[2]
+# f.close()
+
+
+# Reading the input txt file and converting it to a 9x9 numpy array
 sudoku_array = bd.board_to_array(data)
 
 
 print(sudoku_array)
 
+while bt.find_empty(sudoku_array) is not None:
+    empty_box = bt.find_empty(sudoku_array)
+    for guess in range(0, 10):
+        valid = bt.check_validity(sudoku_array, guess, empty_box)
+        if valid:
+            sudoku_array[empty_box[0], empty_box[1]] = guess
+            break
 
-#Sudoku solving algorithms
 
-#Backtracking algorithm
-
-
-
-    
-
-
+print(sudoku_array)
---
+++ test/test_find_empty.py
@@ -0,0 +1,18 @@
+from src.Sudoku import backtracking as bt
+import numpy as np
+
+
+def test_find_empty():
+    """
+    Test for find_empty function in backtracking.py
+
+    """
+
+    # Creating a test sudoku array with no empty boxes (0 as an element)
+    sudoku1 = np.array([[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]])
+
+    # Creating a test sudoku array with one empty box at index (2,2)
+    sudoku2 = np.array([[1, 1, 1, 1, 1, 1], [1, 1, 0, 1, 1, 1]])
+
+    assert bt.find_empty(sudoku1) is None
+    assert bt.find_empty(sudoku2) == (1, 2)
