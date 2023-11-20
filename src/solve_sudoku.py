9ea8e9b34d1f8370617cd3e94f50dc24002ee919
Sabahattin Mert Daloglu <smd89@cam.ac.uk>
2023-11-20 13:58:12 +0000

    added input and output file extraction

--- src/solve_sudoku.py
+++ src/solve_sudoku.py
@@ -1,7 +1,18 @@
 import numpy as np
+import sys
+
+#Extracting the input and the output file from the command line
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with 
 
 
 #Sudoku solving algorithms
 
 #Backtracking algorithm
+
+
+def sudoku_backtraking(sudoku):
+
 
