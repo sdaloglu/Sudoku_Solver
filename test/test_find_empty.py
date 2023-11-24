ad37b0c8efaf4384a631438305072b906c9f844d
Sabahattin Mert Daloglu <smd89@cam.ac.uk>
2023-11-24 17:46:36 +0000

    Edited the docstring for the test_find_empty func

--- test/test_find_empty.py
+++ test/test_find_empty.py
@@ -6,6 +6,10 @@ def test_find_empty():
     """
     Test for find_empty function in backtracking.py
 
+    This function tests the behavior of the find_empty function in the backtracking.py module.
+    It creates two test sudoku arrays, one with no empty boxes and one with one empty box.
+    It then asserts the expected results of calling the find_empty function on these arrays.
+
     """
 
     # Creating a test sudoku array with no empty boxes (0 as an element)
