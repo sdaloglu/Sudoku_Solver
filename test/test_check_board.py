ddf71dc148b79f4434a97be170187bcffbe3abd0
Sabahattin Mert Daloglu <smd89@cam.ac.uk>
2023-12-13 17:13:48 +0300

    Added the last test file for the new function check_board

---
+++ test/test_check_board.py
@@ -0,0 +1,78 @@
+import numpy as np
+from src.sudoku.board import check_board
+
+
+def test_check_board():
+    """
+    Test for check_board function in board.py
+
+    This function tests the behavior of the check_board function in the board.py module.
+    It creates two test sudoku arrays, one valid and one invalid.
+    It then asserts the expected results of calling the check_board function on these arrays.
+
+    """
+
+    # Creating a valid sudoku board
+    valid_board = np.array(
+        [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9],
+        ]
+    )
+
+    # Creating an invalid sudoku board with repeating number 5 in the last column
+    invalid_board1 = np.array(
+        [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 5],
+        ]
+    )
+
+    # Creating an invalid sudoku board with repeating number 9 in the last row
+    invalid_board2 = np.array(
+        [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [9, 0, 0, 0, 8, 0, 0, 7, 9],
+        ]
+    )
+
+    # Creating an invalid sudoku board with repeating number 5 in the first 3x3 box
+    invalid_board3 = np.array(
+        [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 5, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9],
+        ]
+    )
+
+    assert check_board(valid_board) is True
+    assert check_board(invalid_board1) is False
+    assert check_board(invalid_board2) is False
+    assert check_board(invalid_board3) is False
